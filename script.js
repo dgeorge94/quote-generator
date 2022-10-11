a9c8d76100712881d89dcb1aebfd7ef60f3238ee
DGeorge <dalton.george94@outlook.com>
2022-10-11 05:34:40 -0500

    log error

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ async function getQuotes() {
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-        // Catch Error Here
+        console.log(error);
     }
 }
 
